adb767947c0b9c3baad9c7c4477108058a798044
Geoneer - Richard Zijlstra <geoneer@live.nl>
2012-08-15 01:04:55 +0200

    https://github.com/Geonovum/pdok_wizzard/issues/12

--- js/pdok_api.js
+++ js/pdok_api.js
@@ -1,12 +1,8 @@
 ﻿/* File Created: augustus 14, 2012 */
 function pdok_api_map_resize(w,h) {
-    var frame = this.document.getElementById("map");
-    frame.style.height = h+'px';
-    frame.style.width = w + 'px';
-    markers.redraw();
-    var center = mapPDOKKaart.getCenter();
-    var zoomlevel = 2;//  mapPDOKKaart.getZoom();
-    mapPDOKKaart.setCenter(new OpenLayers.LonLat(center.lon, center.lat),zoomlevel);
+    this.document.getElementById("map").style.height = h + 'px';
+    this.document.getElementById("map").style.width = w + 'px';
+    mapPDOKKaart.updateSize();
 }
 
 
