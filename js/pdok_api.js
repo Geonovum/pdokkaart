f909669c5fcfad65f9a4f1d4a226b1af3b80a961
Barent Brouwers <barent.brouwers@gmail.com>
2012-08-22 16:05:47 +0200

    Overbodige javascript en CSS bestanden van de Ordnance Survey opgeruimd.

--- js/pdok_api.js
+++
@@ -1,8 +0,0 @@
-﻿/* File Created: augustus 14, 2012 */
-function pdok_api_map_resize(w,h) {
-    this.document.getElementById("map").style.height = h + 'px';
-    this.document.getElementById("map").style.width = w + 'px';
-    mapPDOKKaart.updateSize();
-}
-
-
