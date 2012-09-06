dc471dcdb46cf239ea5bbad80dc2b0243320d0d1
Richard Duivenvoorde <richard@duif.net>
2012-09-06 15:29:42 +0200

    bb

--- api/javascripts/pdok-api.js
+++ api/javascripts/pdok-api.js
@@ -758,6 +758,10 @@ Pdok.Api.prototype.createOlMap = function() {
 
     // apply layer if a layer was given
     if (this.layer != null) {
+        // if there is just one layer (without comma's), OL returns a String:
+        if (typeof this.layer == 'string') {
+            this.layer=[this.layer];
+        }
         // if the map does NOT have a baseLayer, always add BRT layer
         if (!olMap.baseLayer){
             olMap.addLayer(this.createWMTSLayer( this.defaultLayers.BRT ));
